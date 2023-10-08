04181efa8f6f6274b8e45eb782f7cf6c07ed3c52
Wahdan Najmil <najmilwahdan@gmail.com>
2023-10-08 20:06:02 +0700

    change font

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
 function getData() {
-  fetch('https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user')
-    .then(response => response.json())
-    .then(data => {
+  fetch(
+    "https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user",
+  )
+    .then((response) => response.json())
+    .then((data) => {
       // Urutkan data berdasarkan nomor juz dari angka terkecil ke angka terbesar
       data.sort((a, b) => a.juz - b.juz);
       displayData(data);
     })
-    .catch(error => {
-      console.error('Terjadi kesalahan:', error);
-      alert('Terjadi kesalahan saat mengambil data!');
+    .catch((error) => {
+      console.error("Terjadi kesalahan:", error);
+      alert("Terjadi kesalahan saat mengambil data!");
     });
 }
 
-
 function deleteJuz(juzId) {
   Swal.fire({
-    icon: 'warning',
-    title: 'Anda yakin ingin menghapus data ini?',
+    icon: "warning",
+    title: "Anda yakin ingin menghapus data ini?",
     showCancelButton: true,
-    confirmButtonText: 'Ya, hapus!',
-    cancelButtonText: 'Tidak',
+    confirmButtonText: "Ya, hapus!",
+    cancelButtonText: "Tidak",
   }).then((result) => {
     if (result.isConfirmed) {
-      fetch(`https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`, {
-        method: 'DELETE',
-      })
+      fetch(
+        `https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`,
+        {
+          method: "DELETE",
+        },
+      )
         .then(() => {
           Swal.fire({
-            icon: 'success',
-            title: 'Data berhasil dihapus!',
-            text: 'Data juz dan nama berhasil dihapus.',
+            icon: "success",
+            title: "Data berhasil dihapus!",
+            text: "Data juz dan nama berhasil dihapus.",
           });
           getData(); // Refresh data after deletion
         })
         .catch((error) => {
-          console.error('Terjadi kesalahan:', error);
+          console.error("Terjadi kesalahan:", error);
           Swal.fire({
-            icon: 'error',
-            title: 'Terjadi kesalahan!',
-            text: 'Terjadi kesalahan saat menghapus data!',
+            icon: "error",
+            title: "Terjadi kesalahan!",
+            text: "Terjadi kesalahan saat menghapus data!",
           });
         });
     }
@@ -89,7 +93,7 @@ function deleteJuz(juzId) {
 //           ${juz30Data.isDone ? '<i class="fas  fa-check-circle"></i>' : '<i class="far fa-circle"></i>'}
 //         </span>
 //         Juz: ${juz30Data.juz} ,  ${juz30Data.nama}
-        
+
 //       </p>
 //     </div>
 //     <hr>`;
@@ -98,9 +102,9 @@ function deleteJuz(juzId) {
 //   }
 // }
 function displayData(data) {
-  const dataContainer = document.getElementById('dataContainer');
-  const juzStatsElement = document.getElementById('juzStats');
-  dataContainer.innerHTML = '';
+  const dataContainer = document.getElementById("dataContainer");
+  const juzStatsElement = document.getElementById("juzStats");
+  dataContainer.innerHTML = "";
 
   // Calculate the number of completed and total juz
   const completedJuzCount = data.filter((item) => item.isDone).length;
@@ -114,8 +118,12 @@ function displayData(data) {
 
   // Create a function to generate the HTML for each data item
   function createDataItemHTML(item) {
-const checkboxIcon = item.isDone ? '<i class="fas fa-heart checked"></i>' : '<i class="far fa-heart unchecked"></i>';
-    const juzNamaStyle = item.isDone ? 'text-decoration: line-through; opacity: 0.5;' : '';
+    const checkboxIcon = item.isDone
+      ? '<i class="fas fa-heart checked"></i>'
+      : '<i class="far fa-heart unchecked"></i>';
+    const juzNamaStyle = item.isDone
+      ? "text-decoration: line-through; opacity: 0.5;"
+      : "";
 
     return `
       <div class="d-flex justify-content-between card-icon align-items-center">
@@ -133,15 +141,13 @@ const checkboxIcon = item.isDone ? '<i class="fas fa-heart checked"></i>' : '<i 
     `;
   }
 
-
   // Create and append HTML for each data item
   data.forEach((item) => {
-    const dataItem = document.createElement('div');
+    const dataItem = document.createElement("div");
     dataItem.innerHTML = createDataItemHTML(item);
     dataContainer.appendChild(dataItem);
   });
 }
-
 
 //     dataItem.innerHTML = `
 //     <div class="card-icon">
@@ -156,42 +162,49 @@ const checkboxIcon = item.isDone ? '<i class="fas fa-heart checked"></i>' : '<i 
 
 function toggleDone(juzId) {
   // Fetch the data from the API
-  fetch(`https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`)
+  fetch(
+    `https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`,
+  )
     .then((response) => response.json())
     .then((data) => {
       // Toggle the isDone property
       data.isDone = !data.isDone;
       // Send the updated data to the API
-      fetch(`https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
+      fetch(
+        `https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
         },
-        body: JSON.stringify(data),
-      })
+      )
         .then(() => {
           Swal.fire({
-            icon: 'success',
-            title: 'Berhasil mengubah status!',
-            text: data.isDone ? 'Juz dan Nama telah selesai.' : 'Juz dan Nama belum selesai.',
+            icon: "success",
+            title: "Berhasil mengubah status!",
+            text: data.isDone
+              ? "Juz dan Nama telah selesai."
+              : "Juz dan Nama belum selesai.",
           });
           getData(); // Refresh data after changing status
         })
         .catch((error) => {
-          console.error('Terjadi kesalahan:', error);
+          console.error("Terjadi kesalahan:", error);
           Swal.fire({
-            icon: 'error',
-            title: 'Terjadi kesalahan!',
-            text: 'Terjadi kesalahan saat mengubah status!',
+            icon: "error",
+            title: "Terjadi kesalahan!",
+            text: "Terjadi kesalahan saat mengubah status!",
           });
         });
     })
     .catch((error) => {
-      console.error('Terjadi kesalahan:', error);
+      console.error("Terjadi kesalahan:", error);
       Swal.fire({
-        icon: 'error',
-        title: 'Terjadi kesalahan!',
-        text: 'Terjadi kesalahan saat mengambil data!',
+        icon: "error",
+        title: "Terjadi kesalahan!",
+        text: "Terjadi kesalahan saat mengambil data!",
       });
     });
 }
@@ -202,13 +215,8 @@ function toggleDone(juzId) {
 
 // ... Kode selanjutnya ...
 
-
 // Panggil fungsi getData() saat halaman pertama kali dimuat untuk menampilkan data dari API
 getData();
-
-
-
-
 
 function addJuzAndNamaToAPI(juzData) {
   const juzInput = parseInt(juzData.juz, 10);
@@ -216,56 +224,61 @@ function addJuzAndNamaToAPI(juzData) {
   // Validate the input for Juz
   if (isNaN(juzInput) || juzInput < 1 || juzInput > 30) {
     Swal.fire({
-      icon: 'error',
-      title: 'Input Juz Salah!',
-      text: 'Masukkan angka Juz antara 1 dan 30.',
+      icon: "error",
+      title: "Input Juz Salah!",
+      text: "Masukkan angka Juz antara 1 dan 30.",
     });
     return; // Stop the function if the input is invalid
   }
 
   // Fetch the data from the API
-  fetch('https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user')
-    .then(response => response.json())
-    .then(data => {
-      const isJuzAlreadyExists = data.some(item => item.juz === juzInput);
+  fetch(
+    "https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user",
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const isJuzAlreadyExists = data.some((item) => item.juz === juzInput);
       if (isJuzAlreadyExists) {
         Swal.fire({
-          icon: 'error',
-          title: 'Data sudah diinputkan!',
-          text: 'Data yang sama sudah ada',
+          icon: "error",
+          title: "Data sudah diinputkan!",
+          text: "Data yang sama sudah ada",
         });
       } else {
         // If Juz is unique, add it to the API
-        fetch('https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+        fetch(
+          "https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...juzData, juz: juzInput }), // Update the juz value to the parsed integer
           },
-          body: JSON.stringify({ ...juzData, juz: juzInput }), // Update the juz value to the parsed integer
-        })
-          .then(response => response.json())
-          .then(data => {
-            console.log('Juz dan Nama berhasil ditambahkan:', data);
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Juz dan Nama berhasil ditambahkan:", data);
             Swal.fire({
-              icon: 'success',
-              title: 'Data berhasil dikirim!',
-              text: 'Juz dan Nama berhasil ditambahkan!',
+              icon: "success",
+              title: "Data berhasil dikirim!",
+              text: "Juz dan Nama berhasil ditambahkan!",
             });
             getData(); // Optional: Refresh data after adding a new entry
           })
-          .catch(error => {
-            console.error('Terjadi kesalahan:', error);
+          .catch((error) => {
+            console.error("Terjadi kesalahan:", error);
             Swal.fire({
-              icon: 'error',
-              title: 'Terjadi kesalahan!',
-              text: 'Terjadi kesalahan saat menambahkan Juz dan Nama!',
+              icon: "error",
+              title: "Terjadi kesalahan!",
+              text: "Terjadi kesalahan saat menambahkan Juz dan Nama!",
             });
           });
       }
     })
-    .catch(error => {
-      console.error('Terjadi kesalahan:', error);
-      alert('Terjadi kesalahan saat memeriksa data!');
+    .catch((error) => {
+      console.error("Terjadi kesalahan:", error);
+      alert("Terjadi kesalahan saat memeriksa data!");
     });
 }
 
@@ -273,98 +286,109 @@ function addJuzAndNamaToAPI(juzData) {
 
 function markAsDone(juzId) {
   // Fetch the data from the API
-  fetch(`https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`)
+  fetch(
+    `https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`,
+  )
     .then((response) => response.json())
     .then((data) => {
       // Update the isDone property to true
       data.isDone = true;
       // Send the updated data to the API
-      fetch(`https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
+      fetch(
+        `https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${juzId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
         },
-        body: JSON.stringify(data),
-      })
+      )
         .then(() => {
           Swal.fire({
-            icon: 'success',
-            title: 'Berhasil menandai sudah selesai!',
-            text: 'Juz dan Nama telah ditandai sebagai sudah selesai.',
+            icon: "success",
+            title: "Berhasil menandai sudah selesai!",
+            text: "Juz dan Nama telah ditandai sebagai sudah selesai.",
           });
           getData(); // Refresh data after marking as done
         })
         .catch((error) => {
-          console.error('Terjadi kesalahan:', error);
+          console.error("Terjadi kesalahan:", error);
           Swal.fire({
-            icon: 'error',
-            title: 'Terjadi kesalahan!',
-            text: 'Terjadi kesalahan saat menandai sudah selesai!',
+            icon: "error",
+            title: "Terjadi kesalahan!",
+            text: "Terjadi kesalahan saat menandai sudah selesai!",
           });
         });
     })
     .catch((error) => {
-      console.error('Terjadi kesalahan:', error);
+      console.error("Terjadi kesalahan:", error);
       Swal.fire({
-        icon: 'error',
-        title: 'Terjadi kesalahan!',
-        text: 'Terjadi kesalahan saat mengambil data!',
+        icon: "error",
+        title: "Terjadi kesalahan!",
+        text: "Terjadi kesalahan saat mengambil data!",
       });
     });
 }
 function cancelAllDoneStatus() {
-  const confirmation = confirm('Anda yakin ingin membatalkan semua status selesai?');
+  const confirmation = confirm(
+    "Anda yakin ingin membatalkan semua status selesai?",
+  );
   if (!confirmation) {
     return;
   }
 
-  fetch('https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user')
-    .then(response => response.json())
-    .then(data => {
-      const promises = data.map(item => {
+  fetch(
+    "https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user",
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const promises = data.map((item) => {
         if (item.isDone) {
           item.isDone = false;
-          return fetch(`https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${item.id}`, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
+          return fetch(
+            `https://6445e9fcee791e1e29f332a7.mockapi.io/api/v1/login-register/user/${item.id}`,
+            {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(item),
             },
-            body: JSON.stringify(item),
-          });
+          );
         }
       });
 
       Promise.all(promises)
         .then(() => {
           Swal.fire({
-            icon: 'success',
-            title: 'Status selesai berhasil dibatalkan untuk semua entri!',
-            text: 'Status selesai diubah menjadi belum selesai.',
+            icon: "success",
+            title: "Status selesai berhasil dibatalkan untuk semua entri!",
+            text: "Status selesai diubah menjadi belum selesai.",
           });
           getData(); // Refresh data after cancelling all done statuses
         })
         .catch((error) => {
-          console.error('Terjadi kesalahan:', error);
+          console.error("Terjadi kesalahan:", error);
           Swal.fire({
-            icon: 'error',
-            title: 'Terjadi kesalahan!',
-            text: 'Terjadi kesalahan saat membatalkan status selesai!',
+            icon: "error",
+            title: "Terjadi kesalahan!",
+            text: "Terjadi kesalahan saat membatalkan status selesai!",
           });
         });
     })
-    .catch(error => {
-      console.error('Terjadi kesalahan:', error);
-      alert('Terjadi kesalahan saat memeriksa data!');
+    .catch((error) => {
+      console.error("Terjadi kesalahan:", error);
+      alert("Terjadi kesalahan saat memeriksa data!");
     });
 }
 
-
-const juzForm = document.getElementById('juzForm');
-juzForm.addEventListener('submit', (event) => {
+const juzForm = document.getElementById("juzForm");
+juzForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const juzInput = document.getElementById('juz').value;
-  const namaInput = document.getElementById('nama').value;
+  const juzInput = document.getElementById("juz").value;
+  const namaInput = document.getElementById("nama").value;
   const juzData = {
     juz: juzInput,
     nama: namaInput,
@@ -376,8 +400,8 @@ juzForm.addEventListener('submit', (event) => {
 // Panggil fungsi getData() saat halaman pertama kali dimuat untuk menampilkan data dari API
 getData();
 
-  $(document).ready(function() {
-    $('#imageCarousel').carousel({
-      interval: 3500 // Waktu dalam milidetik (2 detik)
-    });
+$(document).ready(function () {
+  $("#imageCarousel").carousel({
+    interval: 3500, // Waktu dalam milidetik (2 detik)
   });
+});
